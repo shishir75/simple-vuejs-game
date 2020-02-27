f10b25f7dc184a5d3f7e47ab696587664efaa0f5
shishir75 <shishir.srdr16@gmail.com>
2020-02-28 03:43:44 +0600

    Implementing giveUp method

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ var vue = new Vue({
             this.monsterAttacks();
         },
         giveUp: function () {
-
+            this.initialGame();
         },
         calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
