92bd62dbf9bd09ea67dac958e0ce8e21e9b78ccb
shishir75 <shishir.srdr16@gmail.com>
2020-02-28 02:18:51 +0600

    Implementing startGame method

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ var vue = new Vue({
        playerHealth: 100,
        monsterHealth: 100,
        gameIsRunning: false
-   }
+   },
+
+    methods: {
+       startGame: function () {
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+       }
+    }
 
 
 });
